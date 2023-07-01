04fda871d73df31ef1135af365d8d5ea27214ccf
gdejanovic <gorandejanovic@gmail.com>
2023-07-01 23:19:03 +0200

    added display of read text instead of console log

--- src/components/display.component.tsx
+++ src/components/display.component.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import ImageUpload from './imageUpload.component';
 
 export const DisplayComponent = () => {
+    const [ ocrText, setOcrText] = useState('');
     const handleTextExtracted = (text: string) => {
-        console.log('Extracted Text:', text);
+        setOcrText(text);
     };
 
     return (
         <div>
             <h1>Image Upload with OCR</h1>
             <ImageUpload onTextExtracted={handleTextExtracted} />
+            <p>{ocrText}</p>
         </div>
     );
 };
